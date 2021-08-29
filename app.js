6cd14cb4f153bdc53f48f0e138a4e083818001ea
Taklemariam Alazar <teklealazar399@gmail.com>
2021-08-29 16:24:02 +0300

    files updated

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const tasks = require("./routes/task")
+
+const connectDb = require('./db/connect')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+
+// middleware
+app.use(express.json())
+
+// routes
+
+
+app.use(notFound)
+
+
+const port = 3000
+
+const start = async () =>{
+    try {
+        await connectDb(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    } 
+}
+
+start()
