6a8ce2d4b843b80e4678937ed0116c21577e06c4
Eyob Alemu <it.eyob.alemu@gmail.com>
2021-09-01 13:02:55 +0300

    [backend] fix models bugs

--- backend/models/intervention.js
+++ backend/models/intervention.js
@@ -1,20 +1,18 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const InterventionSchema = new mongoose.Schema({
-    cptCode : {
-        type : Number,
-        required : true
-    }
-    description : {
-        type: String,
-        required : true
-    },
-    price : {
-        type : Number,
-        required : true
-    }
-    
-})
+  cptCode: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('Intervention', InterventionSchema)
+module.exports = mongoose.model("Intervention", InterventionSchema);
--- backend/models/users.js
+++ backend/models/users.js
@@ -1,40 +1,58 @@
-const { required } = require('joi');
-const mongoose = require('mongoose');
-
+const { required } = require("joi");
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name : {
-        type: String,
-        required: true,
-    },
-    password : {
-        type: String,
-        required: true
-    },
-    email : {
-        type: String,
-    },
-    role : {
-        type: String,
-        required: true
-    },
-    profileImage : {
-        type: String,
-    },
-    phone : {
-        type: String,
-    },
-    dateOfBirth: {
-        type: Date,
-        required: true
-    },
-    speciality : {
-        type: String,
-    },
-    createdAt : {
-        type: Date,
-        defaultStatus: Date.now
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  profileImage: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  speciality: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    defaultStatus: Date.now,
+  },
+});
 
-module.exports = mongoose.model('User', UserSchema)
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = bcrypt.hashSync(this.password, salt);
+});
+
+UserSchema.methods.createToken = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+};
+
+UserSchema.methods.comparePassword = function (canditatePassword) {
+  return bcrypt.compareSync(canditatePassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
