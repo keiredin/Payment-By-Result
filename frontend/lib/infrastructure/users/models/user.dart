4e148dd64bb3c362d30852929d7bc28604fa682c
keiredin <se.keiredin.aman@gmail.com>
2021-09-09 01:44:28 +0300

    repository and data provider added for user feature

---
+++ frontend/lib/infrastructure/users/models/user.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: file_names
+
+class User {
+  final String? id;
+  final String? name;
+  final String? role;
+  final bool? isActive;
+  final DateTime? dateOfBirth;
+  final DateTime? createdAt;
+  final String? email;
+  final String? password;
+  final String? phoneNumber;
+  final String? speciality;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.role,
+    required this.dateOfBirth,
+    required this.createdAt,
+    this.isActive,
+    this.email,
+    this.password,
+    this.phoneNumber,
+    this.speciality,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      role:  json['role'],
+      isActive:  json['isActive'],
+      id:  json['_id'],
+      name:  json['name'],
+      dateOfBirth:  json['dateOfBirth'],
+      createdAt:  json['createdAt'],
+      email:  json['email'],
+      password:  json['password'],
+      phoneNumber:  json['phoneNumber'],
+      speciality:  json['speciality'],
+    );
+  }
+}
---
+++ frontend/lib/infrastructure/users/repository/user_repository.dart
@@ -0,0 +1,34 @@
+import './../data_providers/user_data_provider.dart';
+import './../models/models.dart';
+
+
+class CourseRepository {
+  final UsersDataProvider dataProvider;
+  CourseRepository(this.dataProvider);
+
+  Future<User> create(User user,String url) async {
+    return this.dataProvider.create(user, url);
+  }
+  Future<User> login(User user,String url) async {
+    return this.dataProvider.login(user, url);
+  }
+
+  Future<User> update(String url, User user) async {
+    return this.dataProvider.updatePassword(url, user);
+  }
+
+  Future<User> fetchById(String url) async {
+    return this.dataProvider.fetchById(url);
+  }
+
+  Future<User> fetchAll(String) async {
+    return this.dataProvider.fetchAll();
+  }
+  Future<User> search() async {
+    return this.dataProvider.search();
+  }
+
+  Future<void> deactivate(String url) async {
+    this.dataProvider.deactivate(url);
+  }
+}
