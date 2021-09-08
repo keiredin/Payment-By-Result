30891cded403628668ac137645b152ccdaecb9bd
Taklemariam Alazar <teklealazar399@gmail.com>
2021-09-08 17:59:19 +0300

    [frontend] Form header, bootmquestion, textFormField integrated

---
+++ frontend/lib/presentation/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+const BACKGROUND_COLOR = Color(0xFFDAFFF6);
+const BLACK_COLOR = Color(0xF00B0908);
+const LIGHT_GREY_COLOR = Color(0xFFBDBDBD);
+const GREY_COLOR = Color(0xFF665E63);
---
+++ frontend/lib/presentation/core/widgets/text_field.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/presentation/colors.dart';
+import 'package:frontend/presentation/core/widgets/custom_textformfield.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    Key? key,
+    required this.hintText,
+    this.controller,
+    this.obscureText = false,
+    this.suffixIcon,
+    this.onTap = null,
+  }) : super(key: key);
+
+  final String hintText;
+  final TextEditingController? controller;
+  final bool obscureText;
+  final Widget? suffixIcon;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: Theme.of(context).textTheme.bodyText1?.copyWith(
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w500,
+              color: LIGHT_GREY_COLOR,
+              fontSize: 18.0,
+            ),
+        suffixIcon: suffixIcon,
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Color(0x00000000),
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Color(0x00000000),
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        filled: true,
+        fillColor: Colors.white,
+      ),
+      onTap: onTap,
+    );
+  }
+}
