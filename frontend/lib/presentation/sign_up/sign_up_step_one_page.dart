0e83bcce9758dc4ede01cfe19b713f1e02951a70
BemnetAbraham <39336042+BemnetAbraham@users.noreply.github.com>
2021-09-09 05:05:31 +0300

    Create sign_up_step_one_page.dart

---
+++ frontend/lib/presentation/sign_up/sign_up_step_one_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/presentation/colors.dart';
+import 'package:frontend/presentation/sign_up/widgets/sign_up_step_one_form.dart';
+
+class SignUpStepOnePage extends StatelessWidget {
+  const SignUpStepOnePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: BACKGROUND_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.05,
+          ),
+          child: SignUpStepOneForm(),
+        ),
+      ),
+    );
+  }
+}
